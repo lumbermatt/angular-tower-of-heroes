95ad4ea0a0b77fee7edde55c1eab5dc821e109dd
Mathieu <msteffanetti@interact.lu>
2016-05-04 23:08:31 +0200

    Step 4: Multiple Components Step 5: Services

---
+++ app/mock-heroes.js
@@ -0,0 +1,17 @@
+System.register([], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var HEROES;
+    return {
+        setters:[],
+        execute: function() {
+            exports_1("HEROES", HEROES = [
+                { "id": 11, "name": "Captain America" },
+                { "id": 12, "name": "Spider-Man" },
+                { "id": 13, "name": "Batman" },
+                { "id": 14, "name": "Superman" },
+            ]);
+        }
+    }
+});
+//# sourceMappingURL=mock-heroes.js.map
---
+++ app/mock-heroes.ts
@@ -0,0 +1,8 @@
+import {Hero} from "./hero";
+
+export var HEROES: Hero[] = [
+    { "id": 11, "name": "Captain America" },
+    { "id": 12, "name": "Spider-Man" },
+    { "id": 13, "name": "Batman" },
+    { "id": 14, "name": "Superman" },
+];
